c9f5db0cce97ee51d0583ec43ae9e2b34693c0a8
xrover123 <xrover@yandex.ru>
2026-05-26 13:06:13 +0300

    Исправил систему сборки для включения всех классов в один архив

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("java")
     id("application")
 }
 
-group = "org.example"
+group = "com.apsida.parus_itapt"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -23,3 +24,20 @@ tasks.test {
 application {
     mainClass.set("com.apsida.parus_itapt.imp_csv.Main")
 }
+tasks.shadowJar {
+    archiveBaseName.set("FileMove")
+    manifest {
+        attributes["Main-Class"] = "com.apsida.parus_itapt.imp_csv.Main"
+    }
+}
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.apsida.parus_itapt.imp_csv.Main"
+    }
+    // Опционально: собираем «fat jar», чтобы включить зависимости
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }) {
+        exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+    }
+}
+
